ada2df4edbc542c1b8e9de921bc2f063145e7f27
gzr7702 <rgraziano.gzr7702@gmail.com>
2020-04-21 08:41:05 -0400

    upload docker

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,17 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="/app"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+#cat ~/info.txt | docker login --username gzr7702 --password-stdin
+docker login --username gzr7702 --password Lippman.1
+docker tag app rgrazianogzr7702/boston_housing/app
+
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
+

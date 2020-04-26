0a5eb96b8a092b6ef9f09495b6a03fca19f8f8a1
gzr7702 <rgraziano.gzr7702@gmail.com>
2020-04-26 08:28:31 -0400

    add upload

---
+++ upload_docker.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+dockerpath="/app"
+
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+#cat ~/info.txt | docker login --username gzr7702 --password-stdin
+docker login --username gzr7702 --password password
+docker tag app rgrazianogzr7702/boston_housing/app
+
+
+# Step 3:
+# Push image to a docker repository
+docker push $dockerpath
+
